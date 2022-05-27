226e9dd1f7d5d1e26e69f88ed09cd5060b429e68
CHARA\meich <mei.charaa@gmail.com>
2022-05-27 12:07:36 +0900

    雨が落ちて消えます

---
+++ Assets/TamuraMei/Scripts/RainFall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainFall : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    [SerializeField] float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.velocity = new Vector2(-0.8f, -1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player" || other.gameObject.tag == "Field")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
