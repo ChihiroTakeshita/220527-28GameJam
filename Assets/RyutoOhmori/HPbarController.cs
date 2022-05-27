e30000bf92764cf0d94643d454790f318e5bd63c
RyutoOhmori <ryutoomori0414@gmail.com>
2022-05-27 12:11:42 +0900

    UI を配置しました。

---
+++ Assets/RyutoOhmori/HPbarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour
+{
+    [SerializeField] float _hp = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RyutoOhmori/TimeController.cs
@@ -0,0 +1,26 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimeController : MonoBehaviour
+{
+    [SerializeField]
+    Text _text = default;
+
+    float _timer = default;
+
+    bool _isStop = default;
+
+    void Update()
+    {
+        if (_isStop) { return; }
+
+        _timer += Time.deltaTime;
+        _text.text = _timer.ToString("f1");
+    }
+
+    public void ControlTime()
+    {
+        _isStop ^= true;
+    }
+}
