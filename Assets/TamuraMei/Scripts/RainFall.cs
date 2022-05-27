0e143011e908a6374825216888e01458a18e6b35
CHARA\meich <mei.charaa@gmail.com>
2022-05-27 14:11:56 +0900

    地面とプレイヤーに当たった後の動作を追加しました

--- Assets/TamuraMei/Scripts/RainFall.cs
+++ Assets/TamuraMei/Scripts/RainFall.cs
@@ -6,10 +6,14 @@ public class RainFall : MonoBehaviour
 {
     Rigidbody2D _rb;
     [SerializeField] float speed = 0;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite hitRain;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = Random.Range(3, 6);
         _rb = GetComponent<Rigidbody2D>();
         _rb.velocity = new Vector2(-0.8f, -1) * speed;
     }
@@ -17,14 +21,22 @@ public class RainFall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < -10 || transform.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player" || other.gameObject.tag == "Field")
         {
-            Destroy(gameObject);
+            spriteRenderer.color = Color.blue;
+            _rb.velocity = new Vector2(-0.1f, 0.3f) * speed;
+            Destroy(gameObject, 0.3f);
+            /*Instantiate(HitRain, transform.position, Quaternion.identity);
+            Destroy(HitRain, 0.2f); ←これでエラー出た　
+            から、spriteRendererを変更してDestroyする形に変更　*/
         }
     }
 }
