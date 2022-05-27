58fcc28f0b2a3695af5ab22921aea30c784cd2a8
ChihiroTakeshita <83819925+ChihiroTakeshita@users.noreply.github.com>
2022-05-27 15:33:17 +0900

    移動速度低下アイテムを取ると移動速度が低下することを確認しました

--- Assets/Takeshita/Scripts/ItemSpawner.cs
+++ Assets/Takeshita/Scripts/ItemSpawner.cs
@@ -18,15 +18,16 @@ public class ItemSpawner : MonoBehaviour
         if( _interval > _frequency )
         {
             GenerateItem();
+            _interval = 0;
         }
     }
 
     private void GenerateItem()
     {
-        if (Random.Range(0, 1) > 0.5f)
+        if (Random.Range(0f, 1f) > 0.5f)
         {
             Debug.Log("アイテムが発生したよ");
-            //Instantiate(_items[Random.Range(0, _items.Count - 1)], new Vector2(Random.Range(_leftEnd.position.x, _rightEnd.position.x), transform.position.y), Quaternion.identity);
+            Instantiate(_items[Random.Range(0, _items.Count - 1)], new Vector2(Random.Range(_leftEnd.position.x, _rightEnd.position.x), transform.position.y), Quaternion.identity);
         }
         else
         {
