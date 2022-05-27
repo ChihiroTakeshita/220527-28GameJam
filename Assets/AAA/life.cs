7368d857c1529e264999a7d895bdc8c18c46524e
PS22B_ATOBE <japanatobe@gmail.com>
2022-05-27 15:40:15 +0900

    タイトル画面の作成

---
+++ Assets/AAA/life.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+public class LifeScript : MonoBehaviour
+{
+
+	RectTransform rt;
+
+	void Start()
+	{
+		rt = GetComponent<RectTransform>();
+	}
+
+	public void LifeDown(int ap)
+	{
+		rt.sizeDelta -= new Vector2(0, ap);
+	}
+	public void LifeUp(int hp)
+	{
+		rt.sizeDelta += new Vector2(0, hp);
+		if (rt.sizeDelta.y > 100f)
+		{
+			rt.sizeDelta = new Vector2(51f, 100f);
+		}
+	}
+}
