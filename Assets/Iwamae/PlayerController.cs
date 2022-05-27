b01ed3925bbfc5b67e8498dc144da1bbc775152c
Cactusism1 <sisimple99.81@gmail.com>
2022-05-27 11:08:25 +0900

    プレイヤーの移動

---
+++ Assets/Iwamae/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("プレイヤーの移動速度")]
+    [SerializeField] float _speed = 5.0f;
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        //float v = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(h, 0);
+        _rb.velocity = dir.normalized * _speed;
+    }
+}
