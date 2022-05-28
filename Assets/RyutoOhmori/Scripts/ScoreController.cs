7f4e7d6a0f970cdaacafdc9586a321907a8d05e1
CHARA\meich <mei.charaa@gmail.com>
2022-05-28 17:58:26 +0900

    スコアがリセットされるようにしました

--- Assets/RyutoOhmori/Scripts/ScoreController.cs
+++ Assets/RyutoOhmori/Scripts/ScoreController.cs
@@ -20,6 +20,7 @@ public class ScoreController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameManager.Instance.StartGame();
         _text.text = "スコア:" + 0;
     }
     // Update is called once per frame
